ffe140127efca856355a5e6f0114714fad47d315
ommadawn46 <ommadawn46@gmail.com>
2019-01-08 02:29:05 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# go-dining-philosophers
+
+[食事する哲学者の問題](https://ja.wikipedia.org/wiki/%E9%A3%9F%E4%BA%8B%E3%81%99%E3%82%8B%E5%93%B2%E5%AD%A6%E8%80%85%E3%81%AE%E5%95%8F%E9%A1%8C)
+
+```bash
+go run dining-philosophers.go -action=0 # deadlock
+go run dining-philosophers.go -action=1
+go run dining-philosophers.go -action=2
+```
---
+++ dining-philosophers.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var eatN = 1000
+var philosopherNames = []string{"Socrates", "Plato", "Aristotle", "Kant", "Nietzsche"}
+
+var actionFlag = flag.Int("action", 0, "select philosophers action")
+
+type philosopher struct {
+	name      string
+	rightFork chan struct{}
+	leftFork  chan struct{}
+
+	action func(*philosopher)
+}
+
+func (p *philosopher) takeRightFork() {
+	fmt.Println(p.name, "is taking a right fork.")
+	<-p.rightFork
+}
+
+func (p *philosopher) takeLeftFork() {
+	fmt.Println(p.name, "is taking a left fork.")
+	<-p.leftFork
+}
+
+func (p *philosopher) returnRightFork() {
+	fmt.Println(p.name, "is returning a right fork.")
+	p.rightFork <- struct{}{}
+}
+
+func (p *philosopher) returnLeftFork() {
+	fmt.Println(p.name, "is returning a left fork.")
+	p.leftFork <- struct{}{}
+}
+
+func (p *philosopher) eat() {
+	fmt.Println(p.name, "is eating.")
+	time.Sleep(time.Duration(rand.Intn(10000)) * time.Nanosecond)
+}
+
+func (p *philosopher) run(done chan struct{}) {
+	for i := 0; i < eatN; i++ {
+		p.action(p)
+	}
+	done <- struct{}{}
+}
+
+func newPhilosopher(name string, action func(*philosopher)) *philosopher {
+	fork := make(chan struct{}, 1)
+	go func() { fork <- struct{}{} }()
+	return &philosopher{
+		name:      name,
+		rightFork: fork,
+		action:    action,
+	}
+}
+
+func dine(names []string, actions []func(*philosopher)) error {
+	if len(names) != len(actions) {
+		return fmt.Errorf("names length and actions length must be same")
+	}
+
+	philosophers := []*philosopher{}
+	for i := 0; i < len(names); i++ {
+		philosophers = append(
+			philosophers,
+			newPhilosopher(
+				names[i], actions[i],
+			),
+		)
+	}
+	for i, philo := range philosophers {
+		if i == len(names)-1 {
+			philosophers[0].leftFork = philo.rightFork
+		} else {
+			philosophers[i+1].leftFork = philo.rightFork
+		}
+	}
+
+	startTime := time.Now()
+	doneChannels := []chan struct{}{}
+	for _, philo := range philosophers {
+		done := make(chan struct{}, 1)
+		doneChannels = append(doneChannels, done)
+		go philo.run(done)
+	}
+	for _, done := range doneChannels {
+		<-done
+	}
+	endTime := time.Now()
+
+	fmt.Printf("dining finished in %.2f seconds.", (endTime.Sub(startTime)).Seconds())
+	return nil
+}
+
+func allTakeRightFirst() {
+	takeRightForkFirst := func(p *philosopher) {
+		p.takeRightFork()
+		p.takeLeftFork()
+		p.eat()
+		p.returnLeftFork()
+		p.returnRightFork()
+	}
+	dine(
+		philosopherNames,
+		[]func(*philosopher){
+			takeRightForkFirst,
+			takeRightForkFirst,
+			takeRightForkFirst,
+			takeRightForkFirst,
+			takeRightForkFirst,
+		},
+	)
+}
+
+func oneTakeLeftFirst() {
+	takeRightForkFirst := func(p *philosopher) {
+		p.takeRightFork()
+		p.takeLeftFork()
+		p.eat()
+		p.returnLeftFork()
+		p.returnRightFork()
+	}
+	takeLeftForkFirst := func(p *philosopher) {
+		p.takeLeftFork()
+		p.takeRightFork()
+		p.eat()
+		p.returnRightFork()
+		p.returnLeftFork()
+	}
+	dine(
+		philosopherNames,
+		[]func(*philosopher){
+			takeLeftForkFirst,
+			takeRightForkFirst,
+			takeRightForkFirst,
+			takeRightForkFirst,
+			takeRightForkFirst,
+		},
+	)
+}
+
+func allAskWaiter() {
+	waiter := make(chan struct{}, len(philosopherNames)-1)
+	waitWaitersOk := func(p *philosopher) {
+		waiter <- struct{}{}
+		p.takeRightFork()
+		p.takeLeftFork()
+		p.eat()
+		p.returnLeftFork()
+		p.returnRightFork()
+		<-waiter
+	}
+	dine(
+		philosopherNames,
+		[]func(*philosopher){
+			waitWaitersOk,
+			waitWaitersOk,
+			waitWaitersOk,
+			waitWaitersOk,
+			waitWaitersOk,
+		},
+	)
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	flag.Parse()
+	switch *actionFlag {
+	case 0:
+		allTakeRightFirst() // !!! this pattern will deadlock !!!
+	case 1:
+		oneTakeLeftFirst()
+	case 2:
+		allAskWaiter()
+	}
+}
